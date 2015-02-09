dea04a569c1359378ce9f018ca1d19e8f4246bba
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-02-09 13:32:15 -0600

    initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+# url_for("static", filename="style.css")
+# url_for("static", filename="logic.js")
+
+@app.route("/", )
+def index():
+    # return "What?"
+    name = {}
+    return render_template('index.html')
+
+@app.route("/findwinners")
+def find_winners():
+	return "Finding winners..."
+
+if __name__ == "__main__":
+    app.run()
