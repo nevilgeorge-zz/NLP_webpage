cfbd373a101ad35a91681c7c086a1e77b4d5df7e
Frank Avino <frankavino@Franks-MacBook-Pro.local>
2015-02-16 00:46:53 -0600

    trying mrjsontable instead, need to adapt it to our json format

--- static/logic.js
+++ static/logic.js
@@ -1,12 +1,5 @@
 // Shorthand for $( document ).ready()
 $(function() {
     console.log( "ready!" );
-    $('#my-final-table').dynatable();
-    console.log( "ready!" );
-});
 
-$('#my-final-table').dynatable({
-  dataset: {
-    records: myRecords
-  }
 });
