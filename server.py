96fdf4e67364ac0ca8b3e4838ae5ae9340bc74e6
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-02-16 00:27:37 -0600

    changing server.py and adding funnytweets.json

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, url_for
+import json
 app = Flask(__name__)
 
 # url_for("static", filename="style.css")
 # url_for("static", filename="logic.js")
 
-@app.route("/", )
+@app.route("/")
 def index():
     # return "What?"
     name = {}
     return render_template('index.html')
 
-@app.route("/findwinners")
-def find_winners():
-	return render_template('table.html')
+@app.route("/jokes")
+def jokes():
+	json_data = open("funnytweets.json")
+	data = json.load(json_data)
+	return render_template('table.html', data=data)
+	
 
 if __name__ == "__main__":
     app.run()
