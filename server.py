dbfe3d77996e574e90cd003c927be7edea7dcdc5
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-02-17 13:36:32 -0600

    adding some server.py changes

--- server.py
+++ server.py
@@ -5,22 +5,16 @@ app = Flask(__name__)
 # url_for("static", filename="style.css")
 # url_for("static", filename="logic.js")
 
-# json_data = open("results.json")
-# data = json.load(json_data)
-# print data
-
 @app.route("/")
 def index():
-    # return "What?"
-    name = {}
     return render_template('index.html')
 
 @app.route("/winners")
 def winners():
 	json_data = open("output15.json")
 	data = json.load(json_data)
+	json_data.close()
 	structured_data = data[u'structured']
-	print structured_data
 	return render_template('winners.html', data=structured_data)
 
 @app.route("/jokes")
